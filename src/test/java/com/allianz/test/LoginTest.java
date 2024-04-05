5a4df05545f7ad8d40f06dfb9a712288c8ccb53d
Vinayamol-Thomas <vinayamolthomas@gmail.com>
2024-04-05 03:41:59 -0700

    add more test methods

--- src/test/java/com/allianz/test/LoginTest.java
+++ src/test/java/com/allianz/test/LoginTest.java
@@ -20,4 +20,14 @@ public class LoginTest extends AutomationWrapper {
 		String actualHeader=driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
 		Assert.assertEquals(actualHeader, "Dashboard");
 	}
+	@Test
+	public void invalidLoginTest()
+	{
+		
+		driver.findElement(By.name("username")).sendKeys("Admi");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		String actualerror=driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
+		Assert.assertEquals(actualerror, "Invalid credentials");
+	}
 }
